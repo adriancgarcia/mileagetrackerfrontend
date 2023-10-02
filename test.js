253d2cc13cb339a831b1d274ff46b050d3484478
adriancgarcia <adriancgarcia@gmail.com>
2023-10-02 01:07:21 -0500

    testing math algo

---
+++ test.js
@@ -0,0 +1,14 @@
+function calcReimbursement() {
+    let tripStart = parseFloat(document.getElementById('tripStart').value);
+    let tripEnd = parseFloat(document.getElementById('tripEnd').value);
+    let tripReimbursed = parseFloat(document.getElementById('tripReimbursed').value);
+    
+    let distance = tripEnd - tripStart;
+
+    let mileage = distance * tripReimbursed;
+
+    document.getElementById('outputDiv').innerHTML = mileage;
+    // console.log(mileage)
+}; 
+
+console.log(mileage)
