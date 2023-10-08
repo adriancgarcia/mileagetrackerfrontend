c88c562daa2683f7587125d2a56c9bc7ab82d4e3
adriancgarcia <adriancgarcia@gmail.com>
2023-10-08 15:58:45 -0500

    added svelte to the frontend and created first form

---
+++ mileagetracker/src/lib/Calculate.svelte
@@ -0,0 +1,26 @@
+<script>
+  
+    function calculate() {
+      let tripStart = parseFloat(document.getElementById('tripStart').value);
+      let tripEnd = parseFloat(document.getElementById('tripEnd').value);
+      let tripReimbursed = parseFloat(document.getElementById('tripReimbursed').value);
+      
+      let distance = tripEnd - tripStart;
+  
+      let mileage = distance * tripReimbursed;
+  
+      document.getElementById('outputDiv').innerHTML = mileage;
+      // console.log(mileage)
+  }; 
+  
+  
+  </script>
+
+Trip Name: <input type="text" id="tripname" placeholder="Trip Name">
+Date of Trip <input type="Date" id="tripdate" placeholder="Date of Trip">
+Trip beginning: <input type="text" id="tripStart" placeholder="Beginning Mileage">
+Trip End: <input type="text" id="tripEnd" placeholder="End Mileage">
+cost per mile <input type="text" id="tripReimbursed" placeholder="Cost Per Mile">
+<button type="button" on:click={calculate}>Calculate Amount</button>
+<br>
+Amount Reimbursed<div id="outputDiv"></div>
